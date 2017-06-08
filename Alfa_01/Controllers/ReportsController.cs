5499b3b002df5d00b896233d05c29aca389ca79f
NET Rápida <netrapida@live.com.pt>
2017-06-08 22:52:56 +0100

    Reports filter

--- Alfa_01/Controllers/ReportsController.cs
+++ Alfa_01/Controllers/ReportsController.cs
@@ -24,27 +24,65 @@ namespace Alfa_1.Controllers
 
 
         // GET: Reports
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(
+            string sortOrder,
+            string currentFilter,
+            string searchString,
+            int? page)
         {
-            var applicationDbContext = _context.Report.Include(r => r.Category);
-            return View(await applicationDbContext.ToListAsync());
+            // bags para a sorting
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["CompleteSortParm"] = String.IsNullOrEmpty(sortOrder) ? "complete" : "";
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+            // CONTEXT -->> incluir applicatioUser do report para mostrar email
+            var reports = from r in _context.Report
+                                            .Include(r => r.Category)
+                                            .Include(u => u.User)
+                          select r;
+            // search stuff
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                reports = reports.Where(s => s.Name.Contains(searchString)
+                                       || s.User.Email.Contains(searchString)
+                                       || s.Category.Name.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "complete_desc":
+                    reports = reports.OrderByDescending(s => s.IsComplete);
+                    break;
+                case "complete":
+                    reports = reports.OrderBy(s => s.IsComplete);
+                    break;
+                case "date_desc":
+                    reports = reports.OrderBy(s => s.Created);
+                    break;
+                default:
+                    reports =reports.OrderByDescending(s => s.Created);
+                    break;
+            }
+
+            int pageSize = 3;
+            return View(await PaginatedList<Report>.CreateAsync(reports.AsNoTracking(), page ?? 1, pageSize));
         }
 
         // GET: Reports/Details/5
         public async Task<IActionResult> Details(long? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
             var report = await _context.Report
                 .Include(r => r.Category)
+                .Include(u => u.User)
                 .SingleOrDefaultAsync(m => m.Id == id);
-            if (report == null)
-            {
-                return NotFound();
-            }
 
             return View(report);
         }
@@ -54,7 +92,7 @@ namespace Alfa_1.Controllers
         {
             ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "Name");
             ViewData["Created"] = DateTime.Now;
-            //ViewData["Created"] = await _manager.GetUserAsync(HttpContext.User);
+           // ViewData["Created"] = await _manager.GetUserAsync(HttpContext.User);
 
             return View();
         }
@@ -64,7 +102,7 @@ namespace Alfa_1.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Description,Img,IsComplete,Latitude,Longitude,CategoryId")] Report report)
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Img,Latitude,Longitude,CategoryId")] Report report)
         {
             if (ModelState.IsValid)
             {
@@ -82,17 +120,10 @@ namespace Alfa_1.Controllers
         // GET: Reports/Edit/5
         public async Task<IActionResult> Edit(long? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
             var report = await _context.Report.SingleOrDefaultAsync(m => m.Id == id);
-            if (report == null)
-            {
-                return NotFound();
-            }
+
             ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "Name", report.CategoryId);
+           // ViewData["UserEmail"] = report.User.Email;
             return View(report);
         }
 
@@ -114,7 +145,7 @@ namespace Alfa_1.Controllers
                 {
                     // report complete check unchecked
                     if (report.IsComplete)
-                    { report.Close = DateTime.Now; }
+                         { report.Close = DateTime.Now; }
                     else { report.Close = default(DateTime); }
 
                     _context.Update(report);
@@ -141,34 +172,34 @@ namespace Alfa_1.Controllers
         }
 
         // GET: Reports/Delete/5
-        public async Task<IActionResult> Delete(long? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Delete(long? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var report = await _context.Report
-                .Include(r => r.Category)
-                .SingleOrDefaultAsync(m => m.Id == id);
-            if (report == null)
-            {
-                return NotFound();
-            }
+        //    var report = await _context.Report
+        //        .Include(r => r.Category)
+        //        .SingleOrDefaultAsync(m => m.Id == id);
+        //    if (report == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(report);
-        }
+        //    return View(report);
+        //}
 
         // POST: Reports/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(long id)
-        {
-            var report = await _context.Report.SingleOrDefaultAsync(m => m.Id == id);
-            _context.Report.Remove(report);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(long id)
+        //{
+        //    var report = await _context.Report.SingleOrDefaultAsync(m => m.Id == id);
+        //    _context.Report.Remove(report);
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction("Index");
+        //}
 
         private bool ReportExists(long id)
         {
