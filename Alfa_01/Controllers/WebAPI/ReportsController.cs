9e066eddb9af422bfc30910bb2f6cace8ae2f0d1
Daniel <Daeynasvistas@gmail.com>
2017-06-12 09:50:46 +0100

    API - first

---
+++ Alfa_01/Controllers/WebAPI/ReportsController.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Alfa_1.Data;
+using Alfa_1.Models;
+
+namespace Alfa_1.Controllers.WebAPI
+{
+    [Produces("application/json")]
+    [Route("api/Reports")]
+    public class ReportsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReportsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Reports
+        [HttpGet]
+        public IEnumerable<Report> GetReport()
+        {
+            var report =  _context.Report;
+
+            return report;
+           // return _context.Report.Include(a => a.Category);
+        }
+
+        // GET: api/Reports/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetReport([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var report = await _context.Report.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(report);
+        }
+
+        // PUT: api/Reports/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReport([FromRoute] long id, [FromBody] Report report)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != report.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(report).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReportExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Reports
+        [HttpPost]
+        public async Task<IActionResult> PostReport([FromBody] Report report)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Report.Add(report);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetReport", new { id = report.Id }, report);
+        }
+
+        // DELETE: api/Reports/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReport([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var report = await _context.Report.SingleOrDefaultAsync(m => m.Id == id);
+            if (report == null)
+            {
+                return NotFound();
+            }
+
+            _context.Report.Remove(report);
+            await _context.SaveChangesAsync();
+
+            return Ok(report);
+        }
+
+        private bool ReportExists(long id)
+        {
+            return _context.Report.Any(e => e.Id == id);
+        }
+    }
+}
